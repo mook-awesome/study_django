0b3bd92a27170ccfbc91057b45ab0e4f8fba2a5b
mook <cygnusx143@gmail.com>
2023-12-11 13:43:00 +0900

    5-10_Django_relationships was completed

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from watchlist_app.api.views import WatchDetailAV, WatchListAV, StreamPlatformAV
+from watchlist_app.api.views import WatchDetailAV, WatchListAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch-list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watch-details'),
-    path('stream/', StreamPlatformAV.as_view(), name='streamplatform')
+    path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamp-detail')
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -21,6 +21,31 @@ class StreamPlatformAV(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class StreamPlatformDetailAV(APIView):
+    
+    def get(self, request, pk):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+            serializer = StreamPlatformSerializer(platform)
+            return Response(serializer.data)
+        except StreamPlatform.DoesNotExist:
+            return Response({'ERROR': 'platform does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
 class WatchListAV(APIView):
     
     def get(self, request):
