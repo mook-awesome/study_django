ffbf21554d3f69bc088d2c99dd8577d6fd61d5b2
mook <cygnusx143@gmail.com>
2023-12-13 13:58:38 +0900

    5-16 URL Structure

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from watchlist_app.api.views import WatchDetailAV, WatchListAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
-    path('list/', WatchListAV.as_view(), name='watch-list'),
-    path('<int:pk>/', WatchDetailAV.as_view(), name='watch-details'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamp-detail'),
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail')
+    path('list/',                   WatchListAV.as_view(),              name='watch-list'),
+    path('<int:pk>',                WatchDetailAV.as_view(),            name='watch-details'),
+    path('stream/',                 StreamPlatformAV.as_view(),         name='stream-list'),
+    path('stream/<int:pk>',         StreamPlatformDetailAV.as_view(),   name='stream-detail'),
+    path('stream/<int:pk>/review',  StreamPlatformDetailAV.as_view(),   name='stream-detail'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(),             name='review-detail'),
 ]
